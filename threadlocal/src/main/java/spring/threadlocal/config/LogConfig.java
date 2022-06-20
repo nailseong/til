c9285cea15ccb0f26cef44e606a6f8c9ba96a322
nailseong <nnis9857@gmail.com>
2022-06-21 01:21:02 +0900

    feat(LogConfig): FieldLogger -> ThreadLocalLogger 빈 변경

--- threadlocal/src/main/java/spring/threadlocal/config/LogConfig.java
+++ threadlocal/src/main/java/spring/threadlocal/config/LogConfig.java
@@ -2,14 +2,14 @@ package spring.threadlocal.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import spring.threadlocal.config.log.FieldLogger;
 import spring.threadlocal.config.log.Logger;
+import spring.threadlocal.config.log.ThreadLocalLogger;
 
 @Configuration
 public class LogConfig {
 
     @Bean
     public Logger logger() {
-        return new FieldLogger();
+        return new ThreadLocalLogger();
     }
 }
